04afc4b018a3ccd00a63a29bab17e0b91f3ac880
Quinten <quinten.roets@gmail.com>
2022-01-24 15:43:51 +0100

    autoformat and sort imports

--- gui.py
+++ gui.py
@@ -1,34 +1,32 @@
-import cli
 import subprocess
+
+import cli
 
 
 def ask(message, choices=None, options=None):
-    options = {'text': f'<big>{message}</big>'} | (options or {})
+    options = {"text": f"<big>{message}</big>"} | (options or {})
 
     if choices is None:
-        res = run('entry', options=options)
+        res = run("entry", options=options)
         res = res and res.strip()
     elif isinstance(choices, list):
         res = ask_choices(choices, options=options)
     elif isinstance(choices, dict):
-        res = ask_choices(
-            list(choices.keys()), options=options
-        )
+        res = ask_choices(list(choices.keys()), options=options)
         res = res and choices[res]
     else:
-        raise Exception('Choices parameter not valid')
+        raise Exception("Choices parameter not valid")
 
     return res
 
 
 def ask_choices(choices, options=None):
-    separator = '###'
-    options = {
-        'separator': separator,
-        'no-headers': None
-    } | (options or {})
-    items = ['--column=text', '--column=@font@'] + [v for c in choices for v in (c, 'Monospace 15')]
-    res = run('list', args=items, options=options)
+    separator = "###"
+    options = {"separator": separator, "no-headers": None} | (options or {})
+    items = ["--column=text", "--column=@font@"] + [
+        v for c in choices for v in (c, "Monospace 15")
+    ]
+    res = run("list", args=items, options=options)
     res = res and res.split(separator)[0]
     return res
 
@@ -36,16 +34,16 @@ def ask_choices(choices, options=None):
 def run(subcommand, args=None, options=None):
     args = args or []
     options = {
-        'geometry': '907x514+500+200',
-        'title': '',
-        'text-align': 'center',
-        'icon-theme': 'Win11',
-        'fontname': 'Noto Sans 40',
+        "geometry": "907x514+500+200",
+        "title": "",
+        "text-align": "center",
+        "icon-theme": "Win11",
+        "fontname": "Noto Sans 40",
     } | (options or {})
-    result = cli.get('yad', f'--{subcommand}', *args, options, check=False)
+    result = cli.get("yad", f"--{subcommand}", *args, options, check=False)
     return result
 
 
 def ask_yn(question):
-    res = subprocess.run(('kdialog', '--yesno', question), capture_output=True)
+    res = subprocess.run(("kdialog", "--yesno", question), capture_output=True)
     return res.returncode == 0
--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-NAME = 'gui'
+import cli
+
+NAME = "gui"
+
 
 def read(filename):
     try:
         with open(filename) as fp:
-            content = fp.read().split('\n')
+            content = fp.read().split("\n")
     except FileNotFoundError:
         content = []
     return content
 
 
 setup(
-    author='Quinten Roets',
-    author_email='quinten.roets@gmail.com',
-    description='gui',
+    author="Quinten Roets",
+    author_email="quinten.roets@gmail.com",
+    description="gui",
     name=NAME,
-    version='1.0',
-	py_modules=['gui'],
-	setup_requires=read('setup_requirements.txt'),
-    install_requires=read('requirements.txt'),
+    version="1.0",
+    py_modules=["gui"],
+    setup_requires=read("setup_requirements.txt"),
+    install_requires=read("requirements.txt"),
 )
 
-import cli
 
-cli.install(*read('packages.txt'))
+cli.install(*read("packages.txt"))
